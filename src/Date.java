0e6674c4476164a18d9d5bcebe513ba4a1772ae0
Sukhjit Singh <ssukhjit1998@gmail.com>
2021-01-27 17:09:48 -0500

    New Project

--- src/Date.java
+++ src/Date.java
@@ -2,7 +2,16 @@ public class Date {
     private int year;
     private int month;
     private int day;
-    public Date(String date) { } //taking mm/dd/yyyy and create a Date object
-    public Date() { } //return today’s date
+    public Date(String date) {
+        this.year = Integer.parseInt(Character.toString(date.charAt(6))+Character.toString(date.charAt(7))+Character.toString(date.charAt(8))+Character.toString(date.charAt(9)));
+        //this.year = Integer.parseInt(date.substring(6));
+        this.day = Integer.parseInt(Character.toString(date.charAt(3))+Character.toString(date.charAt(4)));
+        //this.year = Integer.parseInt(date.substring(3,5));
+        this.month = Integer.parseInt(Character.toString(date.charAt(0))+Character.toString(date.charAt(1)));
+        //this.year = Integer.parseInt(date.substring(0,2));
+    } //taking mm/dd/yyyy and create a Date object
+
+    public Date() {
+    } //return today’s date
     public isValid() { }
 }
