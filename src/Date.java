e9e5f3e560c13107122e75e0e218af8c9f45df92
Sukhjit Singh <ssukhjit1998@gmail.com>
2021-01-27 16:06:53 -0500

    New Project

--- src/Date.java
+++ src/Date.java
@@ -1,2 +1,8 @@
 public class Date {
+    private int year;
+    private int month;
+    private int day;
+    public Date(String date) { } //taking mm/dd/yyyy and create a Date object
+    public Date() { } //return today’s date
+    public isValid() { }
 }
