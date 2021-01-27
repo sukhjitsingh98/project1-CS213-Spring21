6e83d79a25c4f4593b49410a679aa8671e5ddc7d
GermanMunguia <munguiagerman48@gmail.com>
2021-01-27 16:13:14 -0500

    Book

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,16 @@
 public class Book {
+    private String number; //a 5-digit serial number unique to the book
+    private String name;
+    private boolean checkedOut;
+    private Date datePublished;
+
+    @Override
+    public boolean equals(Object obj){
+        return false;
+    }
+    @Override
+    public String toString() {
+        return null;
+    }
+
 }
