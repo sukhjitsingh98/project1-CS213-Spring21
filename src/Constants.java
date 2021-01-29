864cb48d187e31413754adce55b8207933660736
Sukhjit Singh <ssukhjit1998@gmail.com>
2021-01-28 23:02:39 -0500

    Converted magic numbers into constant variables

--- src/Constants.java
+++ src/Constants.java
@@ -1,3 +1,29 @@
 public class Constants {
+    //Constants for Month Names
+    public static final int JANUARY = 1;
+    public static final int FEBRUARY = 2;
+    public static final int MARCH = 3;
+    public static final int APRIL = 4;
+    public static final int MAY = 5;
+    public static final int JUNE = 6;
+    public static final int JULY = 7;
+    public static final int AUGUST = 8;
+    public static final int SEPTEMBER = 9;
+    public static final int OCTOBER = 10;
+    public static final int NOVEMBER = 11;
+    public static final int DECEMBER = 12;
 
+    //Constants for days in a Month
+    public static final int DAYS_IN_LONG_MONTH = 31;
+    public static final int DAYS_IN_SHORT_MONTH = 30;
+    public static final int DAYS_IN_FEB_LEAP_YEAR = 29;
+    public static final int DAYS_IN_FEB_NORMAL_YEAR = 28;
+
+    //Constants for year limits
+    public static final int MINIMUM_YEAR_LIMIT = 1900;
+
+    //Constants for leap year calculation
+    public static final int LEAP_YEAR_INTERVAL = 4;
+    public static final int LEAP_YEAR_CENTURY = 100;
+    public static final int CONFIRM_LEAP_YEAR_CENTURY = 400;
 }
--- src/Date.java
+++ src/Date.java
@@ -4,12 +4,10 @@ public class Date {
     private int year;
     private int month;
     private int day;
+
     public Date(String date) {
-        //this.year = Integer.parseInt(Character.toString(date.charAt(6))+Character.toString(date.charAt(7))+Character.toString(date.charAt(8))+Character.toString(date.charAt(9)));
         this.year = Integer.parseInt(date.substring(6));
-        //this.day = Integer.parseInt(Character.toString(date.charAt(3))+Character.toString(date.charAt(4)));
         this.year = Integer.parseInt(date.substring(3,5));
-        //this.month = Integer.parseInt(Character.toString(date.charAt(0))+Character.toString(date.charAt(1)));
         this.year = Integer.parseInt(date.substring(0,2));
     } //taking mm/dd/yyyy and create a Date object
 
@@ -21,9 +19,9 @@ public class Date {
     } //return today’s date
 
     private Boolean isLeapYear(){
-        if (year%4==0){
-            if (year%100==0){
-                if(year%400==0){
+        if (year%Constants.LEAP_YEAR_INTERVAL==0){
+            if (year%Constants.LEAP_YEAR_CENTURY==0){
+                if(year%Constants.CONFIRM_LEAP_YEAR_CENTURY==0){
                     return true;
                 }
                 else{
@@ -41,40 +39,46 @@ public class Date {
 
     public Boolean isValid() {
         Calendar calendar = Calendar.getInstance();
-        if(year<1900 || year>calendar.get(Calendar.YEAR)){
+        //If year is not within bounds return false
+        if(year<Constants.MINIMUM_YEAR_LIMIT || year>calendar.get(Calendar.YEAR)){
             return false;
         }
         else if(year==calendar.get(Calendar.YEAR)) {
+            //If it is current year, and month is greater than current month return false
             if(month>(calendar.get(Calendar.MONTH)+1)) {
                 return false;
             }
+            //If it is current year and month, and date is greater than current date return false
             else if (month==(calendar.get(Calendar.MONTH)+1)) {
                 if(day>calendar.get(Calendar.DATE)) {
                     return false;
                 }
             }
         }
-        else if(month<1 || month>12) {
+        //If month is not within bounds return false
+        else if(month<Constants.JANUARY || month>Constants.DECEMBER) {
             return false;
         }
-        else if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12){
-            if(day>31 || day<1){
+        //If it is a long month, and day is outside of bounds return false
+        else if(month==Constants.JANUARY || month==Constants.MARCH || month==Constants.MAY || month==Constants.JULY || month==Constants.AUGUST || month==Constants.OCTOBER || month==Constants.DECEMBER){
+            if(day>Constants.DAYS_IN_LONG_MONTH || day<1){
                 return false;
             }
         }
-        else if(month== 4 || month==6 || month==9 || month==11){
-            if(day>30 || day<1){
+        //If it is a short month, and day is outside of bounds return false
+        else if(month==Constants.APRIL || month==Constants.JUNE || month==Constants.SEPTEMBER || month==Constants.NOVEMBER){
+            if(day>Constants.DAYS_IN_SHORT_MONTH || day<1){
                 return false;
             }
         }
-        else if(month==2){
-            //Check for leap year
+        //If month is February determine if it is a leap year and if day is outside of bounds return false
+        else if(month==Constants.FEBRUARY){
             if(isLeapYear() == true){
-                if (day>29 || day<1) {
+                if (day>Constants.DAYS_IN_FEB_LEAP_YEAR || day<1) {
                     return false;
                 }
             }
-            else if(day>28 || day<1){
+            else if(day>Constants.DAYS_IN_FEB_NORMAL_YEAR || day<1){
                 return false;
             }
         }
